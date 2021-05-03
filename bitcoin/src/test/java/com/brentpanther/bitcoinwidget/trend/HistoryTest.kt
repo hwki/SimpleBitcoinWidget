8c56b99906a8c3f4831569d65605d2392db9b545
thrillfall <thrillfall@users.noreply.github.com>
2021-05-03 22:52:51 +0200

    purge old entries after adding new ones§

--- bitcoin/src/test/java/com/brentpanther/bitcoinwidget/trend/HistoryTest.kt
+++ bitcoin/src/test/java/com/brentpanther/bitcoinwidget/trend/HistoryTest.kt
@@ -1,7 +1,6 @@
 package com.brentpanther.bitcoinwidget.trend
 
 import junit.framework.Assert.assertEquals
-import kotlinx.coroutines.delay
 import org.junit.Test
 import java.lang.Thread.sleep
 import java.time.Instant.now
@@ -10,7 +9,7 @@ import java.util.concurrent.TimeUnit
 class HistoryTest {
 
     @Test fun testGetValueAtGivenTime() {
-        var history = History(0);
+        val history = History();
         history.addCoinValue(CoinValue(1000.0, now().toEpochMilli()))
         history.addCoinValue(CoinValue(500.0, now().toEpochMilli() - TimeUnit.MINUTES.toMillis(1)))
         history.addCoinValue(CoinValue(100.0, now().toEpochMilli() - TimeUnit.MINUTES.toMillis(2)))
@@ -20,17 +19,17 @@ class HistoryTest {
 
     @Test
     fun testPurgeOldEntries() {
-        var history = History(TimeUnit.SECONDS.toMillis(1))
+        val history = History(TimeUnit.SECONDS.toMillis(1))
 
         var now = now().toEpochMilli()
         history.addCoinValue(CoinValue(1000.0, now))
         assertEquals(1000.0, history.getValueWhen(now))
         assertEquals(1, history.getHistoryEventCount())
 
-        sleep(TimeUnit.SECONDS.toMillis(1))
+        sleep(TimeUnit.MILLISECONDS.toMillis(1200))
+
         now = now().toEpochMilli()
         history.addCoinValue(CoinValue(2000.0, now))
-        history.purgeOutdatedValues()
         assertEquals(2000.0, history.getValueWhen(now))
         assertEquals(1, history.getHistoryEventCount())
     }
