4d8c8412342bfb4aae7557fc64444eb2f30b3f35
hwki <brent.panther@gmail.com>
2021-10-15 20:58:42 -0500

    Fallback to coingecko for removed exchanges

--- bitcoin/src/main/java/com/brentpanther/bitcoinwidget/db/WidgetDatabase.kt
+++ bitcoin/src/main/java/com/brentpanther/bitcoinwidget/db/WidgetDatabase.kt
@@ -1,12 +1,14 @@
 package com.brentpanther.bitcoinwidget.db
 
 import android.content.Context
+import android.util.Log
 import androidx.preference.PreferenceManager
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
+import com.brentpanther.bitcoinwidget.exchange.Exchange
 
 @Database(version = 3, entities = [Widget::class, Configuration::class], exportSchema = true)
 abstract class WidgetDatabase : RoomDatabase() {
@@ -14,6 +16,8 @@ abstract class WidgetDatabase : RoomDatabase() {
     abstract fun widgetDao() : WidgetDao
 
     companion object {
+
+        private val TAG = WidgetDatabase::class.java.simpleName
 
         private val MIGRATION_1_2 = object : Migration(1, 2) {
             override fun migrate(database: SupportSQLiteDatabase) {
@@ -39,6 +43,11 @@ abstract class WidgetDatabase : RoomDatabase() {
                         PreferenceManager.getDefaultSharedPreferences(context),
                         context.getSharedPreferences("bitcoinwidget", Context.MODE_PRIVATE))
                 }
+
+                override fun onOpen(db: SupportSQLiteDatabase) {
+                    fixRemovedExchanges(db)
+                    super.onOpen(db)
+                }
             }
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
@@ -49,6 +58,24 @@ abstract class WidgetDatabase : RoomDatabase() {
                     .build()
                 INSTANCE = instance
                 instance
+            }
+        }
+
+        private fun fixRemovedExchanges(db: SupportSQLiteDatabase) {
+            val cursor = db.query("SELECT id, exchange FROM Widget ORDER BY id")
+            val allExchanges = Exchange.values().map { it.name }
+            val errored = mutableListOf<Int>()
+            while (cursor.moveToNext()) {
+                val exchange = cursor.getString(1)
+                if (!allExchanges.contains(exchange)) {
+                    val id = cursor.getInt(0)
+                    errored.add(id)
+                    Log.w(TAG, "Widget $id: has invalid exchange: $exchange")
+                }
+            }
+            // fallback to coingecko for any broken exchange
+            for (id in errored) {
+                db.execSQL("UPDATE Widget SET exchange = 'COINGECKO' WHERE id = $id")
             }
         }
     }
